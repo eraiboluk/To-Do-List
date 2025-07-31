a9f12dc069ffd9ac8581b9920819a3934182f0e5
eraiboluk <eraiboluk@gmail.com>
2025-07-31 16:53:19 +0300

    Create TodoItem

---
+++ ToDo/Models/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDo.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
