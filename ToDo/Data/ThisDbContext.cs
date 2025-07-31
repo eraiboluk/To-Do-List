5257aa7ec1f12aa4cc189cc4f6229cdc6b2991c9
eraiboluk <eraiboluk@gmail.com>
2025-07-31 16:54:44 +0300

    Create ThisDbContext.cs

---
+++ ToDo/Data/ThisDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Models;
+
+namespace ToDo.Data
+{
+    public class ThisDbContext : DbContext
+    {
+        public ThisDbContext(DbContextOptions<ThisDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
